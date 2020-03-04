b4b4fc5621216ed6289cc8def273f0337e763133
Victor Jeronimo Age <contato.victorja@gmail.com>
2020-03-04 19:00:48 -0300

    Atualização em q29-upper.hs

--- lista_1/q29-upper.hs
+++ lista_1/q29-upper.hs
@@ -1,6 +1,5 @@
 import Data.Char
 
-upper :: [Char] -> [Char]
 upper str = [toUpper caracter | caracter <- str] 
 
 -- Referencia = https://hackage.haskell.org/package/base-4.12.0.0/docs/Data-Char.html
